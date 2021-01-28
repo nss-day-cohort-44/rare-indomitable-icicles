6f0680b3600b8909f46ad3ec5102ba62313b151b
Danny McCracken <dannymccracken13@yahoo.com>
2021-01-28 16:03:39 -0600

    Continuing to refactor form

--- src/components/categories/CategoryForm.js
+++ src/components/categories/CategoryForm.js
@@ -1,21 +1,20 @@
-import React, { useContext } from "react";
-import { useForm } from "react-hook-form";
+import React, { useContext, useEffect, useState } from "react";
 import { CategoryContext } from "./CategoryProvider"
 
 export const CategoryForm = (props) => {
-    const { register, handleSubmit, reset } = useForm();
-    const { addCategory } = useContext(CategoryContext)
-    
-    
-    const onSubmit = (data) => {
-        if (data) {
-        const newCatObj = {
-            userId: parseInt(localStorage.getItem("app_user_id")),
-            name: data.category
-        }
-        addCategory(newCatObj)
-        }
-        reset("")
+    const { categories, addCategory, getCategories, updateCategory  } = useContext(CategoryContext)
+    const [category, setCategory] = useState({})
+
+    const editMode = props.match.params.hasOwnProperty("id")
+
+    const handleControlledInputChange = (event) => {
+        /*
+            When changing a state object or array, always create a new one
+            and change state instead of modifying current one
+        */
+        const newCategory = Object.assign({}, category)
+        newCategory[event.target.label] = event.target.value
+        setCategory(newCategory)
     }
 
     return(
