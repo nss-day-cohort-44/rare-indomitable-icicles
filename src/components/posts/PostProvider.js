ceceef6f4dbf300fba5b86eb390982b0b2815484
Matthew Machurek <mrjm2005@yahoo.com>
2021-02-22 10:44:58 -0600

    merge issues

--- src/components/posts/PostProvider.js
+++ src/components/posts/PostProvider.js
@@ -1,71 +1,72 @@
-import React, {useState, useEffect, useContext } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import { PostTagContext } from "../postTags/PostTagProvider"
 
 export const PostContext = React.createContext()
 
-export const PostProvider = (props) =>{
+export const PostProvider = (props) => {
     const [posts, setPosts] = useState([])
-    const [post, setPost] = useState({category : {}})
+    const [post, setPost] = useState({ category: {} })
     const [postId, setPostId] = useState(0)
-    const {addPostTag} = useContext(PostTagContext)
+    const { addPostTag } = useContext(PostTagContext)
 
 
-    const  getPosts = () =>{
-        return fetch("http://localhost:8088/posts")
+    const getPosts = () => {
+        return fetch("http://localhost:8000/posts")
             .then(res => res.json())
             .then(setPosts)
     }
 
-    const getPostsByUserId = (userId) =>{
+    const getPostsByUserId = (userId) => {
         userId = localStorage.getItem("rare_user_id")
-        return fetch(`http://localhost:8088/posts?user_id=${userId}`)
+        return fetch(`http://localhost:8000/posts?user_id=${userId}`)
             .then(res => res.json())
             .then(setPosts)
     }
 
-    const getSinglePost = (id) =>{
-        return fetch(`http://localhost:8088/posts/${id}`)
+    const getSinglePost = (id) => {
+        return fetch(`http://localhost:8000/posts/${id}`)
             .then(res => res.json())
             .then(setPost)
     }
 
-    const updatePost = (post) =>{
-        return fetch(`http://localhost:8088/posts/${post.id}`, {
+    const updatePost = (post) => {
+        return fetch(`http://localhost:8000/posts/${post.id}`, {
             method: "PUT",
-            headers:{
+            headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(post)
         })
-        .then(getPosts)
+            .then(getPosts)
     }
 
     const addPost = (post, tags) => {
-        return fetch("http://localhost:8088/posts", {
+        return fetch("http://localhost:8000/posts", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(post)
         })
-        .then((res)=>res.json())
-        .then((res)=>{addPostTag({
-            post_id: res.id,
-            tag_array: tags
-        })
-        console.log(tags)
-        })
-        .then(getPosts)
+            .then((res) => res.json())
+            .then((res) => {
+                addPostTag({
+                    post_id: res.id,
+                    tag_array: tags
+                })
+                console.log(tags)
+            })
+            .then(getPosts)
     }
 
     const deletePost = (id) => {
-        return fetch(`http://localhost:8088/posts/${id}`, {
+        return fetch(`http://localhost:8000/posts/${id}`, {
             method: "DELETE"
         })
             .then(getPosts)
     }
 
-    return(
+    return (
         <PostContext.Provider value={{
             posts, addPost, getPosts, updatePost, deletePost, getSinglePost, getPostsByUserId, post, setPost, postId, setPostId
         }}>
