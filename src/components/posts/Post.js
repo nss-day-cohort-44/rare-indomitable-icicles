6fbf3e603830896d4342018d3002453f6bba1d16
Leo Rondeau <christopherleorondeau@gmail.com>
2021-02-23 22:03:43 -0600

    added author

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -7,28 +7,27 @@ import { HumanDate } from "../utils/HumanDate"
 
 export const Post = ({post, props}) =>{
     // when user provider is provided, if statement will need to be altered to user.id = localstorage.getItem(rare_token)
+    // debugger
+    const postid = props.match.params.postid
+
     if(localStorage.getItem("rare_token")){
         return(
             
             <div className="posts">
-                <div>userid: {post.user_id}</div>
-                <div>category_id: {post.category_id}</div>
                 <Link to={{
-                    pathname: `/posts/${post.id}`,
+                    pathname: `/posts/${post.title}`,
                     state: { chosenPost: post }
                 }}>title: {post.title}</Link>
-                <div>publication_date: {<HumanDate date={Date(post.publication_date)} />}</div>
-                <div>image_url: {post.image_url}</div>
-                <div>content: {post.content}</div>
                 <div>category: {post.category.label}</div>
-                <Link to={{
+                <div>author: {post.rare_user.user.first_name}</div>
+                {/* <Link to={{
                     pathname: `/posts/add_tags/${post.id}`,
                     state: { chosenPost: post }
                 }}>Add Tags</Link>
                 <Link to={{
                     pathname: `/posts/manage_tags/${post.id}`,
                     state: { chosenPost: post }
-                }}>Manage Tags</Link>
+                }}>Manage Tags</Link> */}
             </div>
         )
     } else {
